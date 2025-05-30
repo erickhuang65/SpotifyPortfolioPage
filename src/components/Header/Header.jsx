ed98725e2d88ae53fc5abf56ad290b1e9c28183f
Erick <erickchhuang@gmail.com>
2025-05-29 22:37:35 -0700

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Header.css'
 import headerImage from '../../assets/header2.png';
+import verifyImage from '../../assets/verify.png';
 
 const Header = () => {
   return (
     <div className='header'>
         <div className='header-image'> 
             <img src={headerImage} alt="header image" />
-            <div className='verify-image'><img src="src/assets/verify.png" alt="verified icon" /></div>
+            <div className='verify-image'><img src={verifyImage} alt="verified icon" /></div>
             <div className='verify-text'>Verified Artist</div>
             <div className="header-text">Erick Huang</div>
             <div className="body-text">1,218 monthly listeners</div>
