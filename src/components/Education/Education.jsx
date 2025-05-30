164abaa07551b7da47459712db0757aa12859272
Erick <erickchhuang@gmail.com>
2025-05-29 22:40:29 -0700

    Update Education.jsx

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Education.css'
+import sjsuImage from '../../assets/sjsu.png';
+import cdojoImage from '../../assets/cdojo.png';
 
 const Education = () => {
   return (
     <div className='education-page'>
         <div className='education-content'>
-            <div className='education-image'><img src="src/assets/sjsu.png" alt="NGSP logo" /></div>
+            <div className='education-image'><img src={sjsuImage} alt="sjsu logo" /></div>
             <div className='education-details'>
                 <p>Bachelor of Science in Management Information Systems (Computer Science emphasis)</p>
                 <p>San Jose State University</p>
@@ -14,7 +16,7 @@ const Education = () => {
             </div>
         </div>
         <div className='education-content'>
-            <div className='education-image'><img src="src/assets/cdojo.png" alt="NGSP logo" /></div>
+            <div className='education-image'><img src={cdojoImage} alt="cdojo logo" /></div>
             <div className='education-details'>
                 <p>Software Development Certification</p>
                 <p>Colorado Technical University - via Coding Dojo</p>
